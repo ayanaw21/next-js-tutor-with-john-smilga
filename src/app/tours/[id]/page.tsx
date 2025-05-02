0f177ca3410cd97ab3f81e51bc722ceca871e632
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-05-01 23:56:01 -0700

    feat: add dynamic rout page - see the concepts of image

--- src/app/tours/[id]/page.tsx
+++ src/app/tours/[id]/page.tsx
@@ -1,12 +1,35 @@
-// import { log } from 'console'
-import mapsImg from '../../../../images/maps.jpg'
-console.log(mapsImg)
-function page({params}:{params:{id:string}}) {
-  return (
-    <div>
-      <h1 className='text-4xl'>ID : {params.id}</h1>
-    </div>
-  )
+import mapsImg from "../../../../images/maps.jpg";
+import Image from "next/image";
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
+
+
+console.log(mapsImg);
+function page({ params }: { params: { id: string } }) {
+	return (
+		<div>
+			<h1 className="text-4xl">ID : {params.id}</h1>
+			<section className="flex gap-x-4 mt-4">
+				{/* local image */}
+				<div>
+					<Image
+						src={mapsImg}
+						alt="maps"
+            priority
+						width={192}
+						height={192}
+						className="w-48 h-48"
+					/>
+					<h2>local image</h2>
+				</div>
+				{/* remote image */}
+
+				<div>
+          <Image src={url} alt="tour" width={192} height={192} priority className="w-48 h-48 object-cover rounded" />
+          <h2>remote image</h2>
+        </div>
+			</section>
+		</div>
+	);
 }
 
-export default page
+export default page;
--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -1,5 +1,5 @@
-import { log } from "console";
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 const url = "https://www.course-api.com/react-tours-project";
 
@@ -23,13 +23,29 @@ async function ToursPage() {
 	return (
 		<section>
 			<h1 className="text-3xl mb-4">Tours</h1>
-			{data.map((tour) => {
-				return (
-					<Link key={tour.id} href={`/tours/${tour.id}`} className="hover:text-blue-500">
-						<h2 >{tour.name}</h2>
-					</Link>
-				);
-			})}
+			<div className="grid md:grid-cols-2 gap-8 ">
+				{data.map((tour) => {
+					return (
+						<Link
+							key={tour.id}
+							href={`/tours/${tour.id}`}
+							className="hover:text-blue-500"
+						>
+							<h2>{tour.name}</h2>
+							<div className="relative h-48 mb-2">
+								<Image
+									src={tour.image}
+									alt={tour.name}
+									fill
+									sizes="100vw"
+									priority
+									className="object-cover rounded"
+								/>
+							</div>
+						</Link>
+					);
+				})}
+			</div>
 		</section>
 	);
 }
